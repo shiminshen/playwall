54622b79a5465ac77aac89588eaa0ce37653cfa6
Damon Shen <shi-min.shen@yahooinc.com>
2022-07-20 12:17:51 +0800

    fix: tmp use mock data for games

--- src/gql/resolvers/resolver.js
+++ src/gql/resolvers/resolver.js
@@ -1,7 +1,79 @@
 import { PubSub } from 'graphql-subscriptions'
 
-import mockGames from '../../mockGames.json'
+// import mockGames from '../../mockGames.json'
 import mockQuestions from '../../mockQuestions.json'
+
+const mockGames = {
+  games: [
+    {
+      id: 1,
+      title: 'game1',
+      description: 'game1 description',
+      questions: [
+        {
+          id: 1,
+          type: 'text',
+          content: '我會履行我的職責',
+          answer: 'MOCK',
+          options: [
+            {
+              type: 'text',
+              content: '鬼滅之刃',
+            },
+            {
+              type: 'text',
+              content: '進擊的巨人',
+            },
+            {
+              type: 'text',
+              content: '我的英雄學園',
+            },
+            {
+              type: 'text',
+              content: '航海王',
+            },
+          ],
+        },
+        {
+          id: 1,
+          type: 'text',
+          content: '那天人們想起曾經被支配的恐懼',
+          answer: 'MOCK',
+          options: [
+            {
+              type: 'text',
+              content: '進擊的巨人',
+            },
+            {
+              type: 'text',
+              content: '我的英雄學園',
+            },
+            {
+              type: 'text',
+              content: '鬼滅之刃',
+            },
+            {
+              type: 'text',
+              content: '航海王',
+            },
+          ],
+        },
+      ],
+    },
+    {
+      id: 2,
+      title: 'game2',
+      description: 'game2 description',
+      questions: [],
+    },
+    {
+      id: 3,
+      title: 'game3',
+      description: 'game3 description',
+      questions: [],
+    },
+  ],
+}
 
 const pubsub = new PubSub()
 
